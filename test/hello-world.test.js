af484aadc01c8fc730882c61c2b935b1db36b3cb
Leticia Gonzalez <lgonzalezb@ayesa.com>
2020-12-02 16:19:12 +0000

    fallo en test

--- test/hello-world.test.js
+++ test/hello-world.test.js
@@ -5,7 +5,7 @@ var expect = require('chai').expect;
 describe('Math', function() {
     describe('#abs()', function() {
         it('should return positive value of given negative number', function() {
-            expect(Math.abs(-5)).to.be.equal(5);
+            expect(Math.abs(-5)).to.be.equal(7);
         });
     });
 });
